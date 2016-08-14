e3dbd8c6f1208aa04ae41c9a48ff2214cfa2a60d
Stephen <stephen@homeceuconnection.com>
2016-08-14 14:15:45 -0500

    Updated class

--- Sources/VaporCorsMiddleware.swift
+++ Sources/VaporCorsMiddleware.swift
@@ -2,7 +2,7 @@ import Foundation
 import Vapor
 import HTTP
 
-final class VaporCorsMiddleware: Middleware {
+class VaporCorsMiddleware: Middleware {
     func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let response = try next.respond(to: request)
 
