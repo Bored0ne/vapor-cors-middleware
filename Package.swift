0cb17d232a6af3c84e0c5b4aaa342d33ebcf5255
Stephen <stephen@homeceuconnection.com>
2016-08-14 13:20:58 -0500

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "vapor-cors-middleware",
-    dependencies: [
-      .Package(url: "https://github.com/vapor/vapor.git",
-        majorVersion: 0,
-        minor: 15)
-    ]
+    name: "vapor-cors-middleware"
 )
