864b859c63df8fc5810b62579615b15044cc9662
Stephen <stephen@homeceuconnection.com>
2016-08-14 14:38:59 -0500

    Finally got it working

--- Sources/VaporCorsMiddleware.swift
+++ Sources/VaporCorsMiddleware.swift
@@ -2,8 +2,9 @@ import Foundation
 import Vapor
 import HTTP
 
-class VaporCorsMiddleware: Middleware {
-    func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+public class VaporCorsMiddleware: Middleware {
+    public init(){}
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let response = try next.respond(to: request)
 
         for header in request.headers{
